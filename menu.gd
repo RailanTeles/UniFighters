bfeaf545fa99b2f82c4a4922e1c63d816d38e484
RailanTeles <135670809+RailanTeles@users.noreply.github.com>
2025-10-18 21:58:41 -0300

    fix das imagens

--- menu.gd
+++ menu.gd
@@ -4,9 +4,14 @@ extends Control
 @onready var texto: Label = $texto
 @onready var animacao_menu: AnimationPlayer = $animacaoMenu
 
+@onready var jogar_botao: Button = $menuOpcoes/VBoxContainer/jogarBotao
+@onready var creditos_botao: Button = $menuOpcoes/VBoxContainer/creditosBotao
+@onready var sair_botao: Button = $menuOpcoes/VBoxContainer/sairBotao
+
 func _ready():
 	menu.visible = false
 	menu.modulate.a = 0
+	animacao_menu.animation_finished.connect(_on_animation_finished)
 
 func _input(event):
 	if not menu.visible and event.is_pressed() and not event.is_echo():
@@ -14,6 +19,9 @@ func _input(event):
 		texto.visible = false
 		animacao_menu.play("fade_in_menu")
 
+func _on_animation_finished(anim_name):
+	if anim_name == "fade_in_menu":
+		jogar_botao.grab_focus()
 
 func _on_sair_botao_pressed() -> void:
 	get_tree().quit()
