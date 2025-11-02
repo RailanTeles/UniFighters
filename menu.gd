2e0de71f4d771e63a5c82587b7cce7c061f58f27
RailanTeles <135670809+RailanTeles@users.noreply.github.com>
2025-11-01 22:45:11 -0300

    animação no botão de iniciar

--- menu.gd
+++ menu.gd
@@ -8,6 +8,9 @@ extends Control
 @onready var creditos_botao: Button = $menuOpcoes/VBoxContainer/creditosBotao
 @onready var sair_botao: Button = $menuOpcoes/VBoxContainer/sairBotao
 
+@onready var menu_musica: AudioStreamPlayer = $menuMusica
+@onready var confirmar_musica: AudioStreamPlayer = $confirmarMusica
+
 func _ready():
 	menu.visible = false
 	menu.modulate.a = 0
@@ -24,7 +27,15 @@ func _on_animation_finished(anim_name):
 		jogar_botao.grab_focus()
 
 func _on_jogar_botao_pressed() -> void:
-	pass # Replace with function body.
+	menu_musica.stop()
+	confirmar_musica.play()
+	var tween = create_tween()
+	tween.tween_property(jogar_botao, "modulate:a", 0.0, 0.1)
+	tween.tween_property(jogar_botao, "modulate:a", 1.0, 0.1)
+	tween.tween_property(jogar_botao, "modulate:a", 0.0, 0.1)
+	tween.tween_property(jogar_botao, "modulate:a", 1.0, 0.1)
+	await tween.finished
+	await confirmar_musica.finished
 
 func _on_creditos_botao_pressed() -> void:
 	get_tree().change_scene_to_file("res://creditos.tscn")
