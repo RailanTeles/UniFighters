96a9b8ff00f746ab7842b4d4024743541d47d060
RailanTeles <135670809+RailanTeles@users.noreply.github.com>
2025-11-14 18:08:36 -0300

    luta feita

--- cena_luta.gd
+++ cena_luta.gd
@@ -4,57 +4,156 @@ extends Node2D
 @onready var p2_start_position = $P2_StartPoint
 @onready var icone_p1: TextureRect = $HUD/P1/iconeP1
 @onready var icone_p2: TextureRect = $HUD/P2/iconeP2
-@onready var barra_vida_p1: ColorRect = $HUD/P1/infosP1/BarraVidaP1/VidaP1
 
-# Salva o tamanho máximo da barra
-var max_largura_barra_p1: float
+# --- Referências de Barras ---
+@onready var barra_vida_p1: TextureProgressBar = $HUD/P1/infosP1/barra_vida_p1
+@onready var barra_vida_p2: TextureProgressBar = $HUD/P2/infosP2/barra_vida_p2
+@onready var barra_aura_p1: TextureProgressBar = $HUD/P1/infosP1/AuraP1/barra_aura_p1
+@onready var barra_aura_p2: TextureProgressBar = $HUD/P2/infosP2/AuraP2/barra_aura_p2
 
+# --- Referências dos Indicadores de Vitória ---
+@onready var v1p1: PanelContainer = $HUD/P1/infosP1/VitoriasP1/V1P1
+@onready var v2p1: PanelContainer = $HUD/P1/infosP1/VitoriasP1/V2P1
+@onready var v1p2: PanelContainer = $HUD/P2/infosP2/VitoriasP2/V1P2
+@onready var v2p2: PanelContainer = $HUD/P2/infosP2/VitoriasP2/V2P2
+
+# --- Variáveis de Lógica do Round ---
+var vitorias_p1 = 0
+var vitorias_p2 = 0
+var round_em_andamento = true
+var estilo_vitoria = preload("res://assets/themes/estilo_vitoria.tres") # O arquivo que você criou
+
+# Referências dos jogadores (para podermos resetá-los)
+var p1: CharacterBody2D
+var p2: CharacterBody2D
+
+# (Seu dicionário de ícones)
 var jamylle_textura = preload("res://assets/personagens/regulata/icones/RegulataIconeGrande.png")
-
 var icone_personagens = {
 	"res://regulata.tscn" : jamylle_textura
+	# (Adicione os outros personagens)
 }
 
 func _ready():
+	# 1. Instanciar o mapa
 	var mapa = load(DadosdaPartida.caminho_mapa).instantiate()
 	mapa.z_index = -10
 	add_child(mapa)
 
-	# 2. Instanciar os jogadores
-	var p1 = load(DadosdaPartida.caminho_personagem_p1).instantiate()
-	var p2 = load(DadosdaPartida.caminho_personagem_p2).instantiate()
-	icone_p1.texture = icone_personagens[DadosdaPartida.caminho_personagem_p1]
-	icone_p2.texture = icone_personagens[DadosdaPartida.caminho_personagem_p2]
+	# 2. Instanciar os jogadores (agora salvos nas variáveis da classe)
+	p1 = load(DadosdaPartida.caminho_personagem_p1).instantiate()
+	p2 = load(DadosdaPartida.caminho_personagem_p2).instantiate()
 	
-	# ---- DEFINIR O ID DE CADA UM ----
+	# 3. Lógica dos Ícones
+	if icone_personagens.has(DadosdaPartida.caminho_personagem_p1):
+		icone_p1.texture = icone_personagens[DadosdaPartida.caminho_personagem_p1]
+	if icone_personagens.has(DadosdaPartida.caminho_personagem_p2):
+		icone_p2.texture = icone_personagens[DadosdaPartida.caminho_personagem_p2]
+	
+	# 4. Definir IDs
 	p1.player_id = 1
 	p2.player_id = 2
 	
-	# 3. Salva o tamanho MÁXIMO da barra (o tamanho dela no editor)
-	# (Baseado na sua imagem, o tamanho máximo é 370.0)
-	max_largura_barra_p1 = barra_vida_p1.size.x
-	
-	# 4. CONECTAR O SINAL!
-	# "Quando o 'p1' gritar 'vida_mudou', chame a minha função '_on_p1_vida_mudou'"
+	# 5. CONECTAR TODOS OS SINAIS!
 	p1.vida_mudou.connect(_on_p1_vida_mudou)
-	# (Você fará o mesmo para o p2 com 'p2.vida_mudou.connect...')
-	# ---------------------------------
+	p2.vida_mudou.connect(_on_p2_vida_mudou)
+	p1.aura_mudou.connect(_on_p1_aura_mudou)
+	p2.aura_mudou.connect(_on_p2_aura_mudou)
 	
-	# 3. Adicionar os jogadores à cena
+	# Conecta os NOVOS sinais de "morte"
+	p1.morreu.connect(_on_p1_morreu)
+	p2.morreu.connect(_on_p2_morreu)
+	
+	# 6. Adicionar os jogadores à cena
 	add_child(p1)
 	add_child(p2)
 	
-	# 4. Posicionar os jogadores nos pontos de start
-	# (Fazemos isso DEPOIS de adicionar à cena para que 'global_position' funcione)
+	# 7. Posicionar os jogadores
 	p1.global_position = p1_start_position.global_position
 	p2.global_position = p2_start_position.global_position
 	
-	# 5. A "Apresentação"
+	# 8. A "Apresentação"
 	p1.set_oponente(p2)
 	p2.set_oponente(p1)
+	
+	# 9. Puxa os valores iniciais
+	_on_p1_vida_mudou(p1.vida_atual, p1.vida_max)
+	_on_p2_vida_mudou(p2.vida_atual, p2.vida_max)
+	_on_p1_aura_mudou(p1.aura_atual, p1.aura_max)
+	_on_p2_aura_mudou(p2.aura_atual, p2.aura_max)
 
+# --- Funções de Sinal (Barras de Vida e Aura) ---
+# (As 4 funções _on_pX_vida_mudou e _on_pX_aura_mudou continuam iguais)
 func _on_p1_vida_mudou(vida_atual_nova, vida_max_nova):
+	barra_vida_p1.value = (float(vida_atual_nova) / float(vida_max_nova)) * 100
+func _on_p1_aura_mudou(aura_atual_nova, aura_max_nova):
+	barra_aura_p1.value = (float(aura_atual_nova) / float(aura_max_nova)) * 100
+func _on_p2_vida_mudou(vida_atual_nova, vida_max_nova):
+	barra_vida_p2.value = (float(vida_atual_nova) / float(vida_max_nova)) * 100
+func _on_p2_aura_mudou(aura_atual_nova, aura_max_nova):
+	barra_aura_p2.value = (float(aura_atual_nova) / float(aura_max_nova)) * 100
+
+# --- NOVAS FUNÇÕES: Lógica de Round ---
+
+# Chamada quando o P1 morre (P2 ganha o round)
+func _on_p1_morreu():
+	# Se o round já acabou, não faz nada (evita bugs)
+	if not round_em_andamento:
+		return
 	
-	var porcentagem = float(vida_atual_nova) / float(vida_max_nova)
+	round_em_andamento = false
+	vitorias_p2 += 1 # P2 ganha
+	_atualizar_ui_vitoria(2, vitorias_p2) # Atualiza a UI do P2
 	
-	barra_vida_p1.size.x = max_largura_barra_p1 * porcentagem
+	# Espera 3 segundos e então checa o fim da partida
+	await get_tree().create_timer(3.0).timeout
+	_checar_fim_da_partida()
+
+# Chamada quando o P2 morre (P1 ganha o round)
+func _on_p2_morreu():
+	if not round_em_andamento:
+		return
+	
+	round_em_andamento = false
+	vitorias_p1 += 1 # P1 ganha
+	_atualizar_ui_vitoria(1, vitorias_p1) # Atualiza a UI do P1
+	
+	await get_tree().create_timer(3.0).timeout
+	_checar_fim_da_partida()
+
+# Atualiza os painéis de vitória
+func _atualizar_ui_vitoria(player_id: int, vitorias: int):
+	if player_id == 1:
+		if vitorias == 1:
+			v1p1.set("theme_override_styles/panel", estilo_vitoria)
+		elif vitorias == 2:
+			v2p1.set("theme_override_styles/panel", estilo_vitoria)
+	elif player_id == 2:
+		if vitorias == 1:
+			v1p2.set("theme_override_styles/panel", estilo_vitoria)
+		elif vitorias == 2:
+			v2p2.set("theme_override_styles/panel", estilo_vitoria)
+
+# Checa se o jogo acabou ou se um novo round deve começar
+func _checar_fim_da_partida():
+	if vitorias_p1 == 2 or vitorias_p2 == 2:
+		# Jogo acabou, volta pro menu
+		get_tree().change_scene_to_file("res://menu.tscn")
+	else:
+		# Jogo continua, reseta o round
+		_resetar_round()
+
+# Coloca os jogadores de volta no lugar e reseta o estado deles
+func _resetar_round():
+	p1.resetar_estado()
+	p2.resetar_estado()
+	
+	p1.global_position = p1_start_position.global_position
+	p2.global_position = p2_start_position.global_position
+	
+	# Opcional: Reseta a velocidade caso eles estivessem em knockback
+	p1.velocity = Vector2.ZERO
+	p2.velocity = Vector2.ZERO
+	
+	# Permite que o próximo round comece
+	round_em_andamento = true
--- regulata.gd
+++ regulata.gd
@@ -8,6 +8,7 @@ extends CharacterBody2D
 @onready var hurtbox_shape: CollisionShape2D = $HurtBox/HurtBoxShape
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+
 # Variáveis de Movimento, Pulo, Jogador, Oponente
 var velocidade = 250.0
 var direcao_olhando = 1
@@ -16,8 +17,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 # --- Novas Constantes de Knockback ---
 const KNOCKBACK_FRACO = 80.0
-const KNOCKBACK_FORTE_X = 120.0
-const KNOCKBACK_FORTE_Y = -100.0 # Joga o oponente um pouco para cima
+const KNOCKBACK_FORTE_X = 160.0
+const KNOCKBACK_FORTE_Y = -100.0 
 
 var player_id = 1 
 var oponente: Node2D 
@@ -25,123 +26,117 @@ var oponente: Node2D
 # --- Estatísitcas ---
 var vida_max = 1000.0
 var vida_atual = 1000.0 
-var aura_max = 1000.0
+var aura_max = 500.0
 var aura_atual = 0.0
 
 signal vida_mudou(vida_atual_nova, vida_max_nova)
+signal aura_mudou(aura_atual_nova, aura_max_nova)
+signal morreu
 
 # --- Variáveis de Estado para Ataque ---
-var pode_agir = true      # 'false' durante a animação (trava de movimento)
+var pode_agir = true
 var contador_combo = 0
 var dano_do_golpe = 0
 var golpe_e_forte = false 
+var esta_morto = false
+var esta_em_hitstun = false
 
 func _ready():
-	# Emite o sinal no início para a UI (barra de vida) começar cheia
 	emit_signal("vida_mudou", vida_atual, vida_max)
 
 # -----------------------------------------------------------------
-# _physics_process: Cuida de toda a física
+# _physics_process: (Continua igual)
 # -----------------------------------------------------------------
+# (Todo o seu código _physics_process está perfeito)
 func _physics_process(delta):
-	# --- 1. GRAVIDADE ---
-	# Aplica gravidade se não estiver no chão
+	if esta_morto:
+		if not is_on_floor():
+			velocity.y += gravity * delta
+		velocity.x = 0
+		move_and_slide()
+		return 
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# --- 2. PULO ---
 	var input_pular = "p" + str(player_id) + "_pular"
-	# Só pode pular se puder agir (não estar atacando ou levando dano)
 	if Input.is_action_just_pressed(input_pular) and is_on_floor() and pode_agir:
 		velocity.y = JUMP_VELOCITY
-		contador_combo = 0 # Pular quebra o combo
-		timer_combo.stop() # Pular quebra o combo
+		contador_combo = 0
+		timer_combo.stop()
 
-	# --- 3. MOVIMENTO HORIZONTAL ---
 	var input_agachar = "p" + str(player_id) + "_baixo"
 	var is_crouching = Input.is_action_pressed(input_agachar) and is_on_floor()
 	var input_esquerda = "p" + str(player_id) + "_esquerda"
 	var input_direita = "p" + str(player_id) + "_direita"
 	var direcao_input = Input.get_axis(input_esquerda, input_direita)
 	
-	# Não pode andar se estiver agachado ou não puder agir
-	if direcao_input == 0 or is_crouching or not pode_agir:
-		# Se não puder agir (ex: levando knockback), deixa a física (velocidade) rolar
-		if pode_agir:
-			velocity.x = 0
+	if not pode_agir:
+		velocity.x = lerp(velocity.x, 0.0, 0.1) 
+	elif direcao_input == 0 or is_crouching:
+		velocity.x = 0
 	elif direcao_input == direcao_olhando:
 		velocity.x = direcao_input * velocidade
 	else: 
-		velocity.x = direcao_input * (velocidade * 0.7) # Andar para trás é mais lento
+		velocity.x = direcao_input * (velocidade * 0.7)
 	
-	# Aplica o movimento final
 	move_and_slide()
 
 # -----------------------------------------------------------------
-# _process: Cuida das animações e lógica de virar
+# _process: (Continua igual)
 # -----------------------------------------------------------------
+# (Todo o seu código _process está perfeito)
 func _process(delta):
-	# Se o oponente não foi definido, não faz nada
 	if oponente == null:
 		return 
 
-	# --- LÓGICA DE VIRAR (FLIP) ---
-	# Sempre vira para encarar o oponente
-	if oponente.global_position.x > global_position.x:
-		direcao_olhando = 1
-		sprite.flip_h = false
-	else:
-		direcao_olhando = -1
-		sprite.flip_h = true
+	if not esta_morto:
+		if oponente.global_position.x > global_position.x:
+			direcao_olhando = 1
+			sprite.flip_h = false
+		else:
+			direcao_olhando = -1
+			sprite.flip_h = true
 
-	# --- LÓGICA DE ANIMAÇÃO ---
+	if esta_morto:
+		animation_player.play("receber_dano")
+		return
 
-	# Se não pudermos agir (ex: no meio de um golpe), não fazemos nada
 	if not pode_agir:
 		return
 
-	# Pega os inputs do jogador
 	var input_agachar = "p" + str(player_id) + "_baixo"
 	var is_crouching = Input.is_action_pressed(input_agachar)
 	var input_socar_fraco = "p" + str(player_id) + "_socoFraco"
 	var apertou_soco = Input.is_action_just_pressed(input_socar_fraco)
 	
-	# --- LÓGICA DE ATAQUE (Prioridade Máxima) ---
 	if apertou_soco:
 		pode_agir = false # Trava o personagem
 		
-		# Verifica se estamos em uma janela de combo
 		if contador_combo > 0 and timer_combo.time_left > 0:
-			# Se sim, continua o combo
 			if contador_combo == 1:
 				animation_player.play("golpe_fraco2")
 				contador_combo = 2
 			elif contador_combo == 2:
 				animation_player.play("golpe_fraco3")
-				contador_combo = 0 # Fim do combo
+				contador_combo = 0 
 		else:
-			# Se não, começa um combo novo
-			
-			# Checa se está no ar ou agachado
 			if not is_on_floor():
 				animation_player.play("golpe_fraco_ar")
-				contador_combo = 0 # Ataques no ar não dão combo
+				contador_combo = 0 
 			elif is_crouching:
 				animation_player.play("golpe_fraco_agachado")
-				contador_combo = 0 # Ataques agachados não dão combo
+				contador_combo = 0 
 			else:
-				# Começa o combo no chão
 				animation_player.play("golpe_fraco1")
 				contador_combo = 1
 		
-		# Para qualquer timer de combo que estivesse rodando
 		timer_combo.stop()
 
-	# --- LÓGICA DE MOVIMENTO (Se não estiver atacando) ---
 	elif is_on_floor():
 		if is_crouching:
 			sprite.play("agachar")
-			contador_combo = 0 # Agachar quebra o combo
+			contador_combo = 0 
 			timer_combo.stop()
 		else:
 			var direcao_input = Input.get_axis("p" + str(player_id) + "_esquerda", "p" + str(player_id) + "_direita")
@@ -152,10 +147,9 @@ func _process(delta):
 					sprite.play("andar_frente")
 				else: 
 					sprite.play("andar_tras")
-				contador_combo = 0 # Andar quebra o combo
+				contador_combo = 0 
 				timer_combo.stop()
 	
-	# --- LÓGICA DE ANIMAÇÃO NO AR (Se não estiver atacando) ---
 	elif not is_on_floor():
 		if velocity.y < 0:
 			sprite.play("pular")
@@ -163,118 +157,133 @@ func _process(delta):
 			sprite.play("queda")
 
 # -----------------------------------------------------------------
-# Função set_oponente (Chamada pela cena_luta)
+# Função set_oponente (Continua igual)
 # -----------------------------------------------------------------
 func set_oponente(alvo: Node2D):
-	# Define quem é o nosso alvo
 	oponente = alvo
 
 # -----------------------------------------------------------------
-# Função levar_dano (Atualizada)
+# Função levar_dano (Corrigida)
 # -----------------------------------------------------------------
 func levar_dano(quantidade: int, e_forte: bool, direcao_knockback: int):
-	# Se já estamos levando dano, não faça nada
-	if not pode_agir:
+	# Se já estamos mortos OU JÁ ESTAMOS EM HITSTUN, não faça nada
+	if esta_morto or esta_em_hitstun:
 		return
 
 	# Trava o personagem
 	pode_agir = false
+	esta_em_hitstun = true 
 	
-	# Reseta o nosso próprio combo (se estávamos no meio de um)
+	# Reseta o nosso próprio combo
 	contador_combo = 0
 	timer_combo.stop()
 	
 	# Aplica o dano
 	vida_atual -= quantidade
-	vida_atual = max(0, vida_atual)
-	emit_signal("vida_mudou", vida_atual, vida_max) # Avisa a UI
+	vida_atual = max(0, vida_atual) # Garante que a vida não fique negativa
+	emit_signal("vida_mudou", vida_atual, vida_max)
 	
-	# --- Lógica de Animação e Knockback ---
+	# --- Lógica de Morte ---
+	if vida_atual <= 0:
+		esta_morto = true
+		animation_player.play("receber_dano")
+		emit_signal("morreu")
+		return # Não aplica knockback
+
+	# --- Lógica de Animação e Knockback (se NÃO estiver morto) ---
 	if e_forte:
-		# Toca a animação de "caído" (você precisa criar ela no AnimationPlayer)
-		animation_player.play("caido")
-		# Aplica o knockback forte (para trás e para cima)
+		animation_player.play("receber_dano") 
 		velocity.x = KNOCKBACK_FORTE_X * direcao_knockback
 		velocity.y = KNOCKBACK_FORTE_Y
 	else:
-		# Toca a animação de "receber_dano"
 		animation_player.play("receber_dano")
-		# Aplica o knockback fraco (só para trás)
 		velocity.x = KNOCKBACK_FRACO * direcao_knockback
 
+func ganhar_aura(quantidade):
+	aura_atual += quantidade
+	aura_atual = min(aura_atual, aura_max)
+	emit_signal("aura_mudou", aura_atual, aura_max)
 
 # -----------------------------------------------------------------
-# Funções de Sinal (Conectadas pelo Editor)
+# Funções de Sinal (Corrigidas)
 # -----------------------------------------------------------------
 func _on_timer_combo_timeout() -> void:
-	# Chamada quando os 2s da janela de combo terminam
 	contador_combo = 0
 
 func _on_animation_player_animation_finished(anim_name: StringName):
-	# Chamada quando uma animação do AnimationPlayer termina
-	
+	# Se o personagem está morto, ele NÃO PODE ser destravado
+	if esta_morto:
+		animation_player.play("receber_dano") 
+		return
+
 	# Destrava o personagem
 	pode_agir = true
 	
-	# Lista de animações que ABREM uma janela de combo
+	# Se a animação que terminou foi a de levar dano,
+	# reseta a trava de hitstun
+	if anim_name == "receber_dano":
+		esta_em_hitstun = false
+	
+	# (Sua lógica de combo continua igual)
 	var ataques_combo = ["golpe_fraco1", "golpe_fraco2"]
 	if anim_name in ataques_combo:
-		# Inicia o timer da "janela" para o próximo golpe
 		timer_combo.start()
 	
-	# Lista de animações que RESETAM o combo
 	var ataques_reset = ["golpe_fraco3", "golpe_fraco_ar", "golpe_fraco_agachado"]
 	if anim_name in ataques_reset:
 		contador_combo = 0
 
 # -----------------------------------------------------------------
-# _on_hurtbox_area_entered
+# _on_hurtbox_area_entered (Corrigido)
 # -----------------------------------------------------------------
 func _on_hurt_box_area_entered(area: Area2D) -> void:
-	# Esta função é chamada quando o Hitbox do oponente (area) entra no nosso Hurtbox
-	
-	# 1. Pega o script do oponente (que é o "dono" do hitbox)
+	# A trava 'if not pode_agir' foi removida daqui.
+	# 1. Verifica se a hitbox que entrou é a NOSSA PRÓPRIA.
+	if area.get_owner() == self:
+		return # Se for, ignore o golpe e saia da função.
+		
 	var oponente_script = area.get_owner()
-	
-	# 2. Pega os dados do golpe dele
 	var dano_recebido = oponente_script.dano_do_golpe
 	var e_forte = oponente_script.golpe_e_forte
-	
-	# 3. Calcula a direção do knockback (para onde vamos ser empurrados)
-	# (sign() retorna 1 se for positivo, -1 se for negativo)
 	var direcao_knockback = sign(global_position.x - oponente_script.global_position.x)
 	
-	# 4. Chama nossa função levar_dano com todas as informações
 	levar_dano(dano_recebido, e_forte, direcao_knockback)
 
 # -----------------------------------------------------------------
-# Ativa o Hitbox (Chamada pela Faixa de Função da Animação)
+# Ativa o Hitbox (Continua igual)
 # -----------------------------------------------------------------
 func _ativar_hitbox(dano: int, pos_x: float, pos_y: float, tam_x: float, tam_y: float, forte: bool = false):
-	# Armazena as propriedades do golpe atual
 	dano_do_golpe = dano
-	golpe_e_forte = forte # Armazena se é um golpe forte
-	
-	# Ajusta a posição da hitbox baseado na direção que estamos olhando
+	golpe_e_forte = forte 
 	hitbox_shape.position = Vector2(pos_x * direcao_olhando, pos_y)
-	
-	# Ajusta o tamanho do shape
 	(hitbox_shape.shape as RectangleShape2D).size = Vector2(tam_x, tam_y)
-	
-	# Ativa a detecção
-	hitbox.monitoring = true
+	hitbox.monitorable = true 
 	hitbox_shape.disabled = false
-	print("Hitbox ATIVADO - Dano: ", dano_do_golpe, " | Forte: ", golpe_e_forte)
 
 # -----------------------------------------------------------------
-# Desativa o Hitbox (Chamada pela Faixa de Função da Animação)
+# Desativa o Hitbox (Continua igual)
 # -----------------------------------------------------------------
 func _desativar_hitbox():
-	# Desativa a detecção
-	hitbox.monitoring = false
+	hitbox.monitorable = false
 	hitbox_shape.disabled = true
-	
-	# Zera as propriedades do golpe
 	dano_do_golpe = 0 
 	golpe_e_forte = false
-	print("Hitbox DESATIVADO")
+
+# -----------------------------------------------------------------
+# NOVA FUNÇÃO: Reseta o estado do personagem para um novo round
+# -----------------------------------------------------------------
+func resetar_estado():
+	vida_atual = vida_max
+	aura_atual = 0.0 # Ou o valor inicial que você preferir
+	esta_morto = false
+	pode_agir = true
+	esta_em_hitstun = false
+	contador_combo = 0
+	velocity = Vector2.ZERO
+	
+	# Avisa a UI para resetar as barras
+	emit_signal("vida_mudou", vida_atual, vida_max)
+	emit_signal("aura_mudou", aura_atual, aura_max)
+	
+	# Garante que ele comece na animação correta
+	sprite.play("idle")
